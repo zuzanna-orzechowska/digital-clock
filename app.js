79aaadd29c3bac84c1f063bc29012620bee0c29a
zuzanna-orzechowska <zuzannaorzechowska4@gmail.com>
2023-09-07 14:48:50 +0200

    javascript

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-function clock () {             // COŚ TU NIE DZIAŁA BLEP
+function clock () {            
     let date = new Date();
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
 
-    document.querySelector(".hour").innerHTML = hours;
-    document.querySelector(".minute").innerHTML = minutes;
-    document.querySelector(".second").innerHTML = seconds;
+    document.querySelector("#hour").innerHTML = hours;
+    document.querySelector("#minute").innerHTML = minutes;
+    document.querySelector("#second").innerHTML = seconds;
 }
 
+// clock
 let updateClock = setInterval(clock, 1000);
 
-hours = hours > 12 ? hours % 12 : hours;
+hours = hours > 12 ? hours % 12 : hours;        // TO JEST NIE ZDEFINIOWANE ??? 
 
-if (hours < 10) {
+if (hours < 10) {          // TUTAJ ZERA PRZED NIE WIDAĆ JAK JEST < 10 CHOLIPKA :<
     hours = "0" + hours;
 }
 
@@ -21,8 +22,16 @@ if (minutes < 10) {
     minutes = "0" + minutes;
 }
 
-if (seconds < 10) {
-    seconds = "0" + seconds;
-}
-
 updateClock();
+
+// data
+ let today = new Date();
+ let dayNumber = today.getDate();
+ let yearNumber = today.getFullYear();
+ let dayName = today.toLocaleString("default", {weekday: "long"});
+ let monthName = today.toLocaleString("default", {month: "short"});
+
+ document.querySelector("#day").innerHTML = dayName;
+ document.querySelector("#day-number").innerHTML = dayNumber;
+ document.querySelector("#month").innerHTML = monthName;
+ document.querySelector("#year").innerHTML = yearNumber;
